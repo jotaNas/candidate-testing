b9bf64f5345d56e8a3bba7e80cd5de5680f7b74b
João Nascimento <69321552+jotaNas@users.noreply.github.com>
2022-05-12 21:49:25 -0300

    resolution canditate-test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const fetchUserIds = async () => {
+  return ["john.smith", "sara.lee", "jack.ma"];
+};
 
 const App = () => {
-  
-  const fetchUserIds = async () => {
-    return ["john.smith", "sara.lee", "jack.ma"];
-  };
+  const [users, setUsers] = useState([]);
+  const [status, setStatus] = useState([]);
+  const [usersReceivedEmail, setUsersReceivedEmail] = useState([]);
+
+  useEffect(() => {
+    const fn = async () => {
+      const usersData = await fetchUserIds();
+      setUsers(usersData);
+    };
+    fn();
+  }, []);
+
+  useEffect(() => {
+    const fn = async () => {
+      if (users.length > 0) {
+        for (const user of users) {
+          const statusData = await checkStatus(user);
+          setStatus([...status, statusData]);
+        }
+      }
+    };
+
+    fn();
+  }, [users]);
+
+  useEffect(() => {
+    const fn = async () => {
+      if (status.length > 0) {
+        for (const user of users) {
+          const received = await sendEmail();
+          if (received) {
+            setUsersReceivedEmail([...usersReceivedEmail, user]);
+          }
+        }
+      }
+    };
+
+    fn();
+  }, [status]);
 
   const checkStatus = async (userId) => {
     return Math.random() > 0.8
-      ? { status: "offline", id: userId }
-      : { status: "online", id: userId };
+      ? { status: "online", id: userId }
+      : { status: "offline", id: userId };
   };
 
   const sendEmail = async (userId) => {
@@ -22,11 +61,11 @@ const App = () => {
     Question 1: 
     Find all online users and send them emails. Render the users for which the emails were successfully sent
 
-    Step 1: Load users
+    // Step 1: Load users
     Step 2: Check users online
     Step 3: Send email for whom are online
     Step 4: Render those which the email was successfully sent
-  
+
   */
 
   return (
@@ -35,14 +74,14 @@ const App = () => {
         <div>
           All online users that introductions were sucessfully sent
           <ul>
-            <li>Student 1</li>
-            <li>Student 2</li>
-            <li>Student 3</li>
+            {usersReceivedEmail.map((user) => (
+              <li>{user}</li>
+            ))}
           </ul>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
